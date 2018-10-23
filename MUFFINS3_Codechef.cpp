d592701b6678dbd896aa8e74259b21e51aca2a62
jenarddurai <jenarddurai@gmail.com>
2018-10-23 14:51:26 +0530

    Added solution for MUFFINS3 problem

---
+++ MUFFINS3_Codechef.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main() {
+   int t,size,leftOver;
+   long long int n;
+   cin>>t;
+   while(t--){
+       size=0;
+       leftOver=0;
+       cin>>n;
+       size=n/2+1;
+       std::cout << size << std::endl;
+       
+   }
+}
+
